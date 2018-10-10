7cdb8118132c0427150b869079b515bd8a4f922a
mkeley <43392279+mkeley@users.noreply.github.com>
2018-10-10 01:24:16 -0400

    Update hello.rb

--- 00_hello/hello.rb
+++ 00_hello/hello.rb
@@ -1,1 +1,38 @@
-#write your code here
+# require "hello"
+
+# describe "the hello function" do
+  # it "says hello" do
+    # expect(hello).to eq("Hello!")
+  # end
+# end
+
+# describe "the greet function" do
+  # it "says hello to someone" do
+    # expect(greet("Alice")).to eq("Hello, Alice!")
+  # end
+
+#  it "says hello to someone else" do
+#    expect(greet("Bob")).to eq("Hello, Bob!")
+# end
+#end 
+
+
+ def hello()
+  "Hello!"
+  var = "Hello" + ()
+  puts var
+  end
+end
+
+def greet(Alice)
+  "Hello, #{who}!"
+end
+  var = "Hello, #{who}!"
+  puts var
+  
+def greet(Bob)
+  "Hello, #{who}!"
+  end
+puts var ="Hello, #{who}!"
+ end
+end
