a9ba6905c5f59e50a56cf00ba4f05f936ce8a77d
mkeley <43392279+mkeley@users.noreply.github.com>
2018-10-10 01:32:54 -0400

    Update book.rb

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,25 @@
 class Book
-# write your code here
+  attr_accessor :title
+
+  def title
+    titleize(@title)
+  end
+
+private
+
+  def titleize(title)
+    title_words = title.capitalize.split(" ")
+    title_words.collect { |word| cap(word) }.join(" ")
+  end
+puts  @book.title = "inferno"
+
+  def cap(word)
+    skip_words.include?(word) ? word : word.capitalize
+  end
+
+  def skip_words
+    [ "and", "in", "the", "of", "a", "an" ]
+  end
+
+
 end
